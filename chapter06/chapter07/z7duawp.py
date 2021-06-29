e265600bd78bbe45f7414183ee2d5e7314f5443a
s21004 <s21004@std.it-college.ac.jp>
2021-06-29 07:29:13 +0000

    aaa

---
+++ chapter06/chapter07/z7duawp.py
@@ -0,0 +1,8 @@
+list1 = [1,2,3,4]
+list2 = [5,6,7,8]
+addek = []
+for i in list1;
+    for j in lest2:
+        added.append(i + j)
+
+print(added)
---
+++ chapter06/chapter07/zhxf3uk.py
@@ -0,0 +1,3 @@
+for i in range(o,100):
+    print(i)
+    break
